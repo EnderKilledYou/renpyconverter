014bd1cb731ded9ac3ed5685bf21d168340d4203
Ender <sterlingkooshesh@gmail.com>
2022-07-11 10:01:12 -0500

    Removed comments

--- convert2.js
+++ convert2.js
@@ -15,37 +15,10 @@ module.exports= class Convert2 {
 
     ParseLine() {
         if (this.index >= this.lines.length) return null;
-        const line = this.lines[this.index++]
+        const line = this.lines[this.index++].replace(/##.*/g,'');
+
 
         return {Line: line, Depth: this.CountWhiteSpace(line) / 2}
-        if (line.trim().startsWith("label")) {
-            this.Depth = 0;
-        }
-        if (line.trim().endsWith(":")) {
-            this.Depth++;
-            this.DepthLines.push(line)
-            return {Line: line, Depth: this.Depth}
-        }
-        if (line.trim().toLowerCase().startsWith("return")) {
-            this.Depth--;
-            return {Line: line, Depth: this.Depth}
-        }
-
-
-        if (this.index > 0) {
-            const countWhiteSpace = this.CountWhiteSpace(line)
-            let line1 = this.lines[this.index - 2];
-            if (line1.indexOf(':') < 0) {
-                let previousWhiteSpace = this.CountWhiteSpace(line1)
-                console.log(`Previous: ${previousWhiteSpace} . ${countWhiteSpace}`)
-                if (previousWhiteSpace > countWhiteSpace) {
-
-                    this.Depth--;
-                }
-            }
-        }
-
-        return {Line: line, Depth: this.Depth}
 
     }
 }
